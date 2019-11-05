e4ba8d85a71413b14179cbdc0d6366177c7b179e
dmilic-foi <dmilic@foi.hr>
2019-11-05 13:48:03 +0100

    Created entity User

--- database/src/main/java/hr/foi/air/database/SportDatabase.java
+++ database/src/main/java/hr/foi/air/database/SportDatabase.java
@@ -3,6 +3,8 @@ package hr.foi.air.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(version=1, entities = {}, views = {}, exportSchema = false)
+import hr.foi.air.database.entities.*;
+
+@Database(version=1, entities = {User.class}, views = {}, exportSchema = false)
 public abstract class SportDatabase extends RoomDatabase {
 }
---
+++ database/src/main/java/hr/foi/air/database/entities/User.java
@@ -0,0 +1,114 @@
+package hr.foi.air.database.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
+
+import java.util.Date;
+
+import hr.foi.air.database.converters.DateConverter;
+
+@Entity(tableName = "users")
+@TypeConverters(DateConverter.class)
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    int id;
+
+    String email;
+    String username;
+    String password;
+
+    @ColumnInfo(name="phone_number")
+    String phoneNumber;
+    @ColumnInfo(name="first_name")
+    String firstName;
+    @ColumnInfo(name="last_name")
+    String lastName;
+    @ColumnInfo(name="birth_date")
+    Date birthDate;
+    @ColumnInfo(name="register_date")
+    Date registerDate;
+    @ColumnInfo(name="activation_code")
+    String activationCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
+    }
+}
