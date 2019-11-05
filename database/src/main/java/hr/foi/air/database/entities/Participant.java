520f103db85285b0f0b73a7e0836d4e21c4f63d7
dmilic-foi <dmilic@foi.hr>
2019-11-05 15:06:09 +0100

    Created entity Participant

---
+++ database/src/main/java/hr/foi/air/database/entities/Participant.java
@@ -0,0 +1,29 @@
+package hr.foi.air.database.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.TypeConverters;
+
+import java.util.Date;
+
+import hr.foi.air.database.converters.DateConverter;
+
+@Entity(tableName = "participants", primaryKeys = {"player_id", "event_id"})
+@TypeConverters(DateConverter.class)
+public class Participant {
+    @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "player_id")
+    @ColumnInfo(index = true, name = "player_id")
+    int playerId;
+
+    @ForeignKey(entity = Event.class, parentColumns = "id", childColumns = "event_id")
+    @ColumnInfo(index = true, name = "event_id")
+    int eventId;
+
+    @ForeignKey(entity = Chat.class, parentColumns = "id", childColumns = "chat_id")
+    @ColumnInfo(name = "chat_id")
+    int chatId;
+
+    @ColumnInfo(name = "join_date")
+    Date joinDate;
+}
