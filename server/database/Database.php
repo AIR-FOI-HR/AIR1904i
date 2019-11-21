9ae4efb501bd3e2c0e90ae075c804dffd0e32cc0
dmilic-foi <dmilic@foi.hr>
2019-11-21 16:01:10 +0100

    Created class that manages connections with database

---
+++ server/database/Database.php
@@ -0,0 +1,46 @@
+<?php
+
+class Database {
+    private static $INSTANCE = null;
+    
+    public static function getInstance() {
+        if (DbConn::$INSTANCE == null) {
+            DbConn::$INSTANCE = new DbConn();
+        }
+        return DbConn::$INSTANCE;
+    }
+    
+    private $dbName;
+    private $dbDriver;
+    private $username;
+    private $password;
+    private $host;
+    private $port;
+    private $conn;
+    
+    private function Database() {
+        $dbSettings = parse_ini_file(".database.ini", true);
+        $this->dbName = $dbSettings["dbname"];
+        $this->dbDriver = $dbSettings["driver"];
+        $this->username = $dbSettings["user"];
+        $this->password = $dbSettings["password"];
+        $this->host = $dbSettings["host"];
+        $this->port = $dbSettings["port"];
+    }
+    
+    public function getConnection() {
+        if ($this->conn == null) {
+            $this->conn = new PDO(
+                "$this->dbDriver:host=$this->host;port=$this->port;dbname=$this->dbName",
+                $this->username,
+                $this->password                
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return $this->conn;
+    }
+    
+    public function closeConnection() {
+        $this->conn = null;
+    }
+}
