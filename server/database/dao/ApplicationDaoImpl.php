bb33434f28c6b66556bde98503d202a2b258b9f1
dmilic-foi <dmilic@foi.hr>
2019-11-21 16:01:35 +0100

    Implemented DAO for applications

---
+++ server/database/dao/ApplicationDaoImpl.php
@@ -0,0 +1,154 @@
+<?php
+
+class ApplicationDaoImpl implements ApplicationDao {
+    
+    public function deleteApplications($applications) {
+        if (is_null($applications)) { return -1; }
+        
+        $sql = "DELETE FROM applications WHERE event_id=:eventId AND player_id=:playerId";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':playerId', $playerId);
+        $stmt->bindParam(':eventId', $eventId);
+        
+        foreach ($applications as $a) {
+            
+            if (!($a instanceof Application)) { 
+                $db->closeConnection();
+                return; 
+            }
+            
+            $playerId = $a->getPlayerId();
+            $eventId = $a->getEventId();
+            
+            $stmt->execute();
+        }
+        
+        $db->closeConnection();
+    }
+
+    public function getAllApplications() {
+        $sql = "SELECT * FROM applications";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->execute();
+        
+        $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+        $result = $stmt->fetchAll();
+        
+        $db->closeConnection();
+        
+        return $result;
+    }
+
+    public function getApplicationsByEvent($eventId) {
+        if (!($eventId instanceof int)) { return null; }
+        
+        $sql = "SELECT * FROM applications WHERE event_id=:eventId";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':eventId', $eventId);
+        
+        $stmt->execute();
+        
+        $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+        $result = $stmt->fetchAll();
+        
+        $db->closeConnection();
+        
+        return $result;
+    }
+
+    public function getApplicationsByPlayer($playerId) {
+        if (!($playerId instanceof int)) { return null; }
+        
+        $sql = "SELECT * FROM applications WHERE player_id=:playerId";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':playerId', $playerId);
+        
+        $stmt->execute();
+        
+        $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+        $result = $stmt->fetchAll();
+        
+        $db->closeConnection();
+        
+        return $result;
+        
+    }
+
+    public function insertApplications($applications) {
+        if (is_null($applications)) { return -1; }
+        
+        $sql = "INSERT INTO applications(player_id, event_id) VALUES (:playerId, :eventId)";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':playerId', $playerId);
+        $stmt->bindParam(':eventId', $eventId);
+        
+        $ids = [];
+        
+        foreach ($applications as $a) {
+            
+            if (!($a instanceof Application)) { 
+                $db->closeConnection();
+                return -1; 
+            }
+            
+            $playerId = $a->getPlayerId();
+            $eventId = $a->getEventId();
+            
+            $stmt->execute();
+            $ids[] = $conn->lastInsertId();
+        }
+        
+        $db->closeConnection();
+        return $ids;
+    }
+
+    public function updateApplications($applications) {
+        if (is_null($applications)) { return -1; }
+        
+        $sql = "UPDATE applications"
+                . "SET event_id=:eventId, player_id=:playerId "
+                . "WHERE event_id=:eventId AND player_id=:playerId";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':playerId', $playerId);
+        $stmt->bindParam(':eventId', $eventId);
+        
+        foreach ($applications as $a) {
+            
+            if (!($a instanceof Application)) { 
+                $db->closeConnection();
+                return; 
+            }
+            
+            $playerId = $a->getPlayerId();
+            $eventId = $a->getEventId();
+            
+            $stmt->execute();
+        }
+        
+        $db->closeConnection();
+    }
+}
