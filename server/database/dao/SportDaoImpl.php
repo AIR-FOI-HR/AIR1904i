63101ef41af9c9628fe005d6fb5aab263383decf
dmilic-foi <dmilic@foi.hr>
2019-11-21 17:52:44 +0100

    Implemented DAO for sports

---
+++ server/database/dao/SportDaoImpl.php
@@ -0,0 +1,155 @@
+<?php
+
+require_once './interfaces/SportDao.php';
+require_once './Database.php';
+
+class SportDaoImpl implements SportDao {
+    
+    public function deleteSports($sports) {
+        if (is_null($sports)) { return; }
+        
+        $sql = "DELETE FROM sports WHERE id=:id";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':id', $id);
+        
+        foreach ($sports as $s) {
+            
+            if (!($s instanceof Sport)) { 
+                $db->closeConnection();
+                return; 
+            }
+            
+            $id = $s->getId();
+            $stmt->execute();
+        }
+        
+        $db->closeConnection();    
+        
+    }
+
+    public function getAllSports() {
+        $sql = "SELECT * FROM sports";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->execute();
+        
+        $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+        $result = $stmt->fetchAll();
+        
+        $db->closeConnection();
+        
+        return $result;
+    }
+
+    public function getSportById($sportId) {
+        $sql = "SELECT * FROM sports WHERE id=:userId";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':sportId', $sportId);
+        
+        $stmt->execute();
+        
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $result = $stmt->fetch();
+        
+        $db->closeConnection();
+        
+        return $result;        
+    }
+
+    public function insertSports($sports) {
+        if (is_null($sports)) { return -1; }
+        
+        $sql = "INSERT INTO sports("
+                . "name, "
+                . "description, "
+                . "min_players, "
+                . "max_players"
+                . ") VALUES ("
+                . ":name, "
+                . ":description, "
+                . ":minPlayers, "
+                . ":maxPlayers"
+                . ")";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':minPlayers', $minPlayers);
+        $stmt->bindParam(':maxPlayers', $maxPlayers);
+        
+        $ids = [];
+        
+        foreach ($sports as $s) {
+            
+            if (!($s instanceof Sport)) { 
+                $db->closeConnection();
+                return -1; 
+            }
+            
+            $name = $s->getName();
+            $description = $s->getDescription();
+            $minPlayers = $s->getMinPlayers();
+            $maxPlayers = $s->getMaxPlayers();
+            
+            $stmt->execute();
+            $ids[] = $conn->lastInsertId();
+        }
+        
+        $db->closeConnection();
+        return $ids;
+    }
+
+    public function updateSports($sports) {
+        if (is_null($sports)) { return; }
+        
+        $sql = "UPDATE sports SET "
+                . "name=:name, "
+                . "description=:description, "
+                . "min_players=:minPlayers, "
+                . "max_players=:maxPlayers "
+                . "WHERE id=:id";
+        
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':minPlayers', $minPlayers);
+        $stmt->bindParam(':maxPlayers', $maxPlayers);
+        
+        foreach ($sports as $s) {
+            
+            if (!($s instanceof Sport)) { 
+                $db->closeConnection();
+                return;
+            }
+            
+            $id = $s->getId();
+            $name = $s->getName();
+            $description = $s->getDescription();
+            $minPlayers = $s->getMinPlayers();
+            $maxPlayers = $s->getMaxPlayers();
+            
+            $stmt->execute();
+        }
+        
+        $db->closeConnection();
+    }
+
+}
