2b58cf41fdbc84fa4b7226dfabaf0d546de65fa7
dmilic-foi <dmilic@foi.hr>
2019-11-30 17:26:48 +0100

    RestController: Implemented loading data for all entities

--- server/ws/RestController.php
+++ server/ws/RestController.php
@@ -15,7 +15,7 @@ class RestController {
 
     public function getData() {
         $request = $this->processRequest();
-        
+        //var_dump($request); //DEBUG
         $data = [];
         switch($request["method"]) {
             case "GET": $data = $this->loadData($request); break;
@@ -33,9 +33,7 @@ class RestController {
         $reqMethod = filter_input(INPUT_SERVER, "REQUEST_METHOD");
         $reqFormat = isset($_REQUEST["format"]) ? $_REQUEST["format"] : "json";
         $reqEntity = $_REQUEST["entity"];
-        $reqArgs   = json_decode($_REQUEST["args"]);
-        //$reqEntity = filter_input(INPUT_REQUEST, "entity");
-        //$reqArgs   = json_decode(filter_input(INPUT_REQUEST, "args"));
+        $reqArgs   = json_decode(base64_decode($_REQUEST["args"]));
         
         $request = [
             "method" => $reqMethod,
@@ -48,31 +46,31 @@ class RestController {
 
     private function loadData($request) {
         $data = [];
-        
+        $fn = null;
         switch($request["entity"]) {
             case "users": 
                 $data = $this->loadUserData($request["args"]);
                 break;
             case "events": 
-                $this->db->getEventDao();
+                $data = $this->loadEventData($request["args"]);
                 break;
             case "sports": 
-                $this->db->getSportDao();
+                $data = $this->loadSportData($request["args"]);
                 break;
             case "settings": 
-                $this->db->getSettingDao();
+                $data = $this->loadSettingData($request["args"]);
                 break;
             case "applications": 
-                $this->db->getApplicationDao();
+                $data = $this->loadApplicationData($request["args"]);
                 break;
             case "participations": 
-                $this->db->getParticipationDao();
+                $data = $this->loadParticipationData($request["args"]);
                 break;
             case "chats": 
-                $this->db->getChatDao();
+                $data = $this->loadChatData($request["args"]);
                 break;
             case "messages": 
-                $this->db->getMessageDao();
+                $data = $this->loadMessageData($request["args"]);
                 break;
             default:
                 $data = ["error" => "Entity does not exist"]; 
@@ -83,19 +81,139 @@ class RestController {
     
     private function loadUserData($args) {
         $dao = $this->db->getUserDao();
-        
         $data = [];
+        
         if (empty($args)) {
             $data = $dao->getAllUsers();
-        } else if (isset($args["id"])) {
-            $data = $dao->getUserById($data["id"]);
-        } else if (isset($args["email"])) {
-            $data = $dao->getUserByEmail($data["email"]);
-        } else if (isset($args["username"])) {
-            $data = $dao->getUserByUsername($data["username"]);
+        } else if (isset($args->id)) {
+            $data = $dao->getUserById($args->id);
+        } else if (isset($args->email)) {
+            $data = $dao->getUserByEmail($args->email);
+        } else if (isset($args->username)) {
+            $data = $dao->getUserByUsername($args->username);
         } else {
             $data = ["error" => "Invalid arguments"];
         }
+        
+        return $data;
+    }
+
+    public function loadEventData($args) {
+        $dao = $this->db->getEventDao();
+        $data = [];
+        
+        if (empty($args)) {
+            $data = $dao->getAllEvents();
+        } else if (isset($args->id)) {
+            $data = $dao->getEventById($args->id);
+        } else if (isset($args->creatorId)) {
+            $data = $dao->getEventsByCreator($args->creatorId);
+        } else if (isset($args->sportId)) {
+            $data = $dao->getEventsBySport($args->sportId);
+        } else {
+            $data = ["error" => "Invalid arguments"];
+        }
+        
+        return $data;
+    }
+
+    public function loadSportData($args) {
+        $dao = $this->db->getSportDao();
+        $data = [];
+        
+        if (empty($args)) {
+            $data = $dao->getAllSports();
+        } else if (isset($args->id)) {
+            $data = $dao->getSportById($args->id);
+        } else {
+            $data = ["error" => "Invalid arguments"];
+        }
+        
+        return $data;
+    }
+
+    public function loadSettingData($args) {
+        $dao = $this->db->getSettingDao();
+        $data = [];
+        
+        if (empty($args)) {
+            $data = $dao->getAllSettings();
+        } else if (isset($args->id)) {
+            $data = $dao->getSettingById($args->id);
+        } else if (isset($args->userId)) {
+            $data = $dao->getSettingsByUser($args->userId);
+        } else {
+            $data = ["error" => "Invalid arguments"];
+        }
+        
+        return $data;
+    }
+
+    public function loadApplicationData($args) {
+        $dao = $this->db->getApplicationDao();
+        $data = [];
+        
+        if (empty($args)) {
+            $data = $dao->getAllApplications();
+        } else if (isset($args->playerId)) {
+            $data = $dao->getApplicationsByPlayer($args->playerId);
+        } else if (isset($args->eventId)) {
+            $data = $dao->getApplicationsByEvent($args->eventId);
+        } else {
+            $data = ["error" => "Invalid arguments"];
+        }
+        
+        return $data;
+    }
+
+    public function loadParticipationData($args) {
+        $dao = $this->db->getParticipationDao();
+        $data = [];
+        
+        if (empty($args)) {
+            $data = $dao->getAllParticipations();
+        } else if (isset($args->playerId)) {
+            $data = $dao->getParticipationsByPlayer($args->playerId);
+        } else if (isset($args->eventId)) {
+            $data = $dao->getParticipationsByEvent($args->eventId);
+        } else {
+            $data = ["error" => "Invalid arguments"];
+        }
+        
+        return $data;
+    }
+
+    public function loadChatData($args) {
+        $dao = $this->db->getChatDao();
+        $data = [];
+        
+        if (empty($args)) {
+            $data = $dao->getAllChats();
+        } else if (isset($args->id)) {
+            $data = $dao->getChatById($args->id);
+        } else {
+            $data = ["error" => "Invalid arguments"];
+        }
+        
+        return $data;
+    }
+
+    public function loadMessageData($args) {
+        $dao = $this->db->getMessageDao();
+        $data = [];
+        
+        if (empty($args)) {
+            $data = $dao->getAllMessages();
+        } else if (isset($args->id)) {
+            $data = $dao->getMessageById($args->id);
+        } else if (isset($args->senderId)) {
+            $data = $dao->getMessagesBySender($args->senderId);
+        } else if (isset($args->chatId)) {
+            $data = $dao->getMessagesByChat($args->chatId);
+        } else {
+            $data = ["error" => "Invalid arguments"];
+        }
+        
         return $data;
     }
 
