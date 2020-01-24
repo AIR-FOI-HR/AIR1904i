5a8d288410cae93f928fe8236b510f75b7fe0b45
ivan-srsen <ivan.srsen@enterwell.net>
2020-01-24 21:31:05 +0100

    Partially implemented webservice services and handlers

---
+++ client/webservice/src/main/java/com/example/webservice/ISportifyApi.java
@@ -0,0 +1,22 @@
+package com.example.webservice;
+
+import com.example.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * The interface Sportify api.
+ */
+public interface ISportifyApi {
+
+    /**
+     * Gets users.
+     *
+     * @return the users
+     */
+    @GET("users/list/")
+    Call<List<User>> getUsers();
+}
